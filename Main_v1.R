015f25744cf9ea8fb2802a55e9315616fb5dc34a
JTheTriHard <jgronovi@ucsd.edu>
2019-11-07 13:09:12 +0100

    Updated troubleshooting for PCA.

--- Main_v1.R
+++ Main_v1.R
@@ -2,7 +2,6 @@
 # Test Version
 
 # Install necessary packages
-#install.packages("ggplot2",dependencies = TRUE)
 #install.packages("Hmisc",dependencies = TRUE)
 #install.packages("psych",dependencies = TRUE)
 #install.packages("stargazer", dependencies = TRUE)
@@ -10,7 +9,8 @@
 #install.packages("tidyverse",dependencies = TRUE)
 #install.packages("devtools", dependencies = TRUE)
 #install.packages("plotly",dependencies = TRUE)
-install.packages("plyr",dependencies = TRUE)
+#install.packages("plyr",dependencies = TRUE)
+
 # Load data
 dsBikeContract <- read.csv(file="~/Rcode/EUR/BA Final Project/EUR_BA_ResearchProject/Data/BikeSharingContracts.csv",stringsAsFactors=FALSE)
 nInitialObs <- nrow(dsBikeContract)
@@ -108,10 +108,10 @@ cat("Proportion of Variance Not Accounted for: ",pcaSummary$importance[2,4])
 # Biplot for first two components
 biplot(pcaResults)
 # observation 99 is a significant outlier. Age is very similar to PC1 and neighborhood (random) is very similar to PC2
-# TO DO: Figure out how to display lines on 3D plot?
+# TO DO: Figure out how to display var lines on 3D plot?
 
 # WARNING: The fourth component accounts for a significant portion of the variance (~20%)
-# Preferred that first 3 components accounts for at least 90% 
+# TO DO: Test other methods for determining if PCA is appropriate
 # Continue for the sake of practicing the concept, but do not rely on for accurate analysis
 
 # Plot on 2D plot
@@ -128,12 +128,23 @@ plot_ly(newCoords,x = ~PC1, y = ~PC2, z = ~PC3,
                 marker = list(size = 3))
 uniqueCoords3D <- sum(!duplicated(newCoords[,1:3]))
 
-# WARNING: Checking for unique points in any combination of dimensionals always results in 175, can be indicating issue
+
+
+# TROUBLE SHOOTING
+
+# Checking for unique points in any combination of dimensionals for the PCA result always results in 175, can be indicating issue
 uniqueCoords<-plyr::count(newCoords[,1:3])
-sum(test$freq)
 cat("Number of Points Not Displayed Due to Overlap: ",nRows - nrow(uniqueCoords))
 
-
+# Explore original data to check for possible causes of issues
+plot_ly(dsBikeContract,x = ~Age, y = ~HrPWork, z = ~dGender,
+        marker = list(size = 3))
+reducedDF <- dsBikeContract[c("Age","HrPWork","dGender")]
+unique_OG <- plyr::count(reducedDF)
+# There are only 33 unique points out of the 412
+# when only considering Age,Work,Gender (Neighborhood was randomized, and thus left out)
+# The majority form lines in the 3D space, due to the scales of the var
+# Thus likely the cause of the planes occurring in the PCA plot
 
 
 #---------------------------------------------------
