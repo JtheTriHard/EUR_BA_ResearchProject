2df9d106b015c1e2aa64c070d0061024c49aed8d
JTheTriHard <jgronovi@ucsd.edu>
2019-11-24 20:16:42 +0100

    Major update after project file crashed and was unable to push recent updates.

--- Main_v1.R
+++ Main_v1.R
@@ -12,12 +12,12 @@
 #install.packages("plyr",dependencies = TRUE)
 #install.packages("fpc", dependencies = TRUE)
 #install.packages("dbscan", dependencies = TRUE)
+set.seed(123)
 
 # Load data
-dsBikeContract <- read.csv(file="~/Rcode/EUR/BA Final Project/EUR_BA_ResearchProject/Data/BikeSharingContracts.csv",stringsAsFactors=FALSE)
-nInitialObs <- nrow(dsBikeContract)
-
-set.seed(123)
+dsAll <- read.csv(file="~/Rcode/EUR/BA Final Project/EUR_BA_ResearchProject/Data/BikeSharingContracts.csv",stringsAsFactors=FALSE)
+initialObsContract <- sum(dsAll$Contract==TRUE)
+initialObsNo <- sum(dsAll$Contract==FALSE)
 
 #---------------------------------------------------
 #
@@ -25,68 +25,128 @@ set.seed(123)
 #
 #---------------------------------------------------
 
-# Remove observations with NaN (3 removed)
-dsBikeContract <- na.omit(dsBikeContract)
+# Remove respondents not aware of Swapfiets
 
-# Find rows/columns of dataset
-nRows <- nrow(dsBikeContract)
-nCols <- ncol(dsBikeContract)
-cat("Number of Responses Removed: ",nInitialObs-nRows)
+# Remove respondents not living in the Netherlands
 
-# EXPLANATORY VARS
-
-# Create likert indicator as an average of the scores
-myPsychOwn <- c("PsychOwn01", "PsychOwn02", "PsychOwn03","PsychOwn04")
-dsBikeContract$PsychOwn <- psych::alpha(dsBikeContract[myPsychOwn],check.keys=TRUE,cumulative = FALSE)$scores
-
-# Frequency of use (Scale 1-5)
-dsBikeContract$UseFreq <- sample(1:5,nRows, replace = TRUE) # randomly assigned for testing purposes
-
-# Minimum length of contract (1, 3, or 6 months)
-dsBikeContract$MinLength <- sample(seq(from = 0, to = 6, by = 3),nRows, replace = TRUE) # randomly assigned for testing purposes
-for (i in 1:nRows){
-  if (dsBikeContract$MinLength[i]==0){
-    dsBikeContract$MinLength[i] <- 1
-  }
-}
+# Remove respondents with Gender = Other (Insignificant sample size)
 
 # CONTROL VARS
 
 # Age (Survey requests yr of birth)
 curYr = 2019
-dsBikeContract$Age <- curYr - dsBikeContract$YrBirth
+dsContract$Age <- curYr - dsContract$YrBirth
 
-# Employment (1 = NA, 2 = Part-Time, 3 = Full-Time)
+# Enrollment Status
+# Check n of respondents that are not students
+ggplot2::ggplot(dsContract)
+# Remove non-student respondents if not many
+dsContract$Enroll <- TRUE
+
+# Employment (Survey asks for hrs worked per week)
+# (1 = NA, 2 = Part Time, 3 = Full Time)
 for (i in 1:nRows){
-  if (dsBikeContract$HrPWork[i] >= 35){
-    dsBikeContract$HrPWork[i] <- 3
+  if (dsContract$HrPWork[i] >= 35){
+    dsContract$HrPWork[i] <- 3
   }
-  else if (dsBikeContract$HrPWork[i] <35 && dsBikeContract$HrPWork[i] > 0){
-    dsBikeContract$HrPWork[i] <- 2
+  else if (dsContract$HrPWork[i] <35 && dsContract$HrPWork[i] > 0){
+    dsContract$HrPWork[i] <- 2
   }
   else{
-    dsBikeContract$HrPWork[i] <- 1
+    dsContract$HrPWork[i] <- 1
   }
 }
 
 # Location (See report for assignments)
-dsBikeContract$Neighborhood <- sample(1:12,nRows, replace = TRUE) #randomly assigned for testing purposes
-#dsBikeContract$Neighborhood <- dsBikeContract$cLocate
+dsContract$Neighborhood <- dsContract$cLocate
 
 # Gender (1 = M, 2 = F): dGender
 
+# Education Level
+dsContract$Education <- sample(1:6, nRows, replace = TRUE) # randomly assigned for testing purposes
+
+# Employment (1 = NA, 2 = Part-Time, 3 = Full-Time)
+
+# EXPLANATORY VARS
+
+# Create likert indicator as an average of the scores
+allPsychOwn <- c("PsychOwn01", "PsychOwn02", "PsychOwn03","PsychOwn04")
+dsContract$PsychOwn <- psych::alpha(dsContract[allPsychOwn],check.keys=TRUE,cumulative = FALSE)$scores
+
+# Create likert indicator as an average of the scores
+allEnv <- c("PsychOwn01", "PsychOwn02", "PsychOwn03","PsychOwn04")
+dsContract$PsychOwn <- psych::alpha(dsContract[myPsychOwn],check.keys=TRUE,cumulative = FALSE)$scores
+
+# Minimum length of contract (1, 3, or 6 months)
+dsContract$MinLength <- sample(seq(from = 0, to = 6, by = 3),nRows, replace = TRUE) # randomly assigned for testing purposes
+for (i in 1:nRows){
+  if (dsContract$MinLength[i]==0){
+    dsContract$MinLength[i] <- 1
+  }
+}
+
+# Frequency of use
+
+# IF SWAPFIETS CONTRACT
+# Type of Swapfiets
+dsContract$Bike <- sample(1:3,nRows, replace = TRUE) # randomly assigned for testing purposes
+
+# Regular Use (0 = TRUE, 1 = FALSE)
+dsContract$UseFreq <- sample(0:1,nRows, replace = TRUE) # randomly assigned for testing purposes
+
+# Length of Current Contract
+dsContract$Contract <- sample(1:5,nRows, replace = TRUE) # randomly assigned for testing purposes
+
+
 # TARGET VAR
-# Willingness to engage in bike-sharing = IntentContract
+
+# Intent to extend contract
+dsContract$Extend <- sample(0:1,nRows, replace = TRUE) # randomly assigned for testing purposes
+
+# Justification
+dsContract$Extend <- sample(0:4,nRows, replace = TRUE) # randomly assigned for testing purposes
 
 # Remove all unused variables from test dataset
-dsBikeContract <- dplyr::select(dsBikeContract,PsychOwn,UseFreq,MinLength,Age,HrPWork,Neighborhood,dGender,IntentContract)
+dsContract <- dplyr::select(dsContract,PsychOwn,UseFreq,MinLength,Age,HrPWork,Neighborhood,dGender,IntentContract)
+
+# IF SWAPFIETS CONTRACT
+# Type of Swapfiets (Control)
+dsContract$Bike <- sample(1:3,nRows, replace = TRUE) # randomly assigned for testing purposes
+
+# ID var types
+numVars <- c()
+catVars <- c()
+# Create set of respondents with contract
+dsContract <- dplyr::filter(dsAll, dsAll$Contract == TRUE)
+
+# Remove NaN entries
+# Find rows/columns of datasets
+nRows <- nrow(dsContract) # num of observations
+nCols <- ncol(dsContract) # num of vars
+cat("Number of responses removed from customer dataset: ",initialObsContract-nRows,"\n")
+
+
+
+# IF NO SWAPFIETS CONTRACT
+
+# Numerical vars
+numVars2 <- c()
+catVars2 <- c()
+
+
+# Create set of respondents without contract
+dsNoContract <- dplyr::filter(dsAll, dsAll$Contract == FALSE)
+
+# Remove respondents with NaN entries
+dsContract <- na.omit(dsContract)
 
 # Save summary of non-normalized data
-stargazer::stargazer(dsBikeContract,
+stargazer::stargazer(dsContract,
                      align = TRUE ,
                      digits=2,
                      type = "html",
                      out = "~/Rcode/EUR/BA Final Project/EUR_BA_ResearchProject/Results/dsSummary.doc")
+
 
 
 #---------------------------------------------------
@@ -95,10 +155,12 @@ stargazer::stargazer(dsBikeContract,
 #
 #---------------------------------------------------
 
+
+
 # Reduce only the control variables down to a total of 3 dimensions to allow for 3D visualization
 # Leave explanatory vars attached
 
-ctrlVars <- dplyr::select(dsBikeContract,Age,HrPWork,Neighborhood,dGender)
+ctrlVars <- dplyr::select(dsContract,Age,HrPWork,Neighborhood,dGender)
 pcaResults <- prcomp(ctrlVars,scale = TRUE)
 pcaSummary <- summary(pcaResults)
 # Scree plot
@@ -110,7 +172,6 @@ cat("Proportion of Variance Not Accounted for: ",pcaSummary$importance[2,4])
 
 # Biplot for first two components
 biplot(pcaResults)
-# observation 55, 99 is a significant outlier
 # TO DO: Figure out how to display var lines on 3D plot?
 
 # WARNING: The fourth component accounts for a significant portion of the variance (~20%)
@@ -125,11 +186,12 @@ ggplot(newCoords, aes(x=PC1,y=PC2))+
   geom_point(color = "red")
 uniqueCoords2D <- sum(!duplicated(newCoords[,1:2])) 
 
+
 # Plor on 3D plot
 # Generates a plot with roughly 4 groups of planes and ~6 outliers
 library(plotly)
 plot_ly(newCoords,x = ~PC1, y = ~PC2, z = ~PC3,
-                marker = list(size = 3))%>%
+        marker = list(size = 3))%>%
   layout(title = 'Survey Data After PCA')
 uniqueCoords3D <- sum(!duplicated(newCoords[,1:3]))
 
@@ -138,19 +200,21 @@ uniqueCoords3D <- sum(!duplicated(newCoords[,1:3]))
 # TROUBLE SHOOTING
 
 # Checking for unique points in any combination of dimensionals for the PCA result always results in 175, can be indicating issue
-uniqueCoords<-plyr::count(newCoords[,1:3])
+uniqueCoords<-plyr::count(newCoords[,1])
 cat("Number of Points Not Displayed Due to Overlap: ",nRows - nrow(uniqueCoords))
 
 # Explore original data to check for possible causes of issues
-plot_ly(dsBikeContract,x = ~Age, y = ~HrPWork, z = ~dGender,
+plot_ly(dsContract,x = ~Age, y = ~HrPWork, z = ~dGender,
         marker = list(size = 3))%>%
   layout(title = 'Original Survey Data, Not Accounting For Neighborhood')
-reducedDF <- dsBikeContract[c("Age","HrPWork","dGender")]
+reducedDF <- dsContract[c("Age","HrPWork","dGender")]
 unique_OG <- plyr::count(reducedDF)
-# There are only 33 unique points out of the 412
-# when only considering Age,Work,Gender (Neighborhood was randomized, and thus left out)
-# The majority form lines in the 3D space, due to the scales of the var
-# Thus likely the cause of the planes occurring in the PCA plot (dependency within control vars?)
+
+
+
+# FAMD
+
+
 
 
 #---------------------------------------------------
@@ -177,31 +241,31 @@ plot(k.values, wss_values,
      xlab="Number of clusters K",
      ylab="Total within-clusters sum of squares")
 
-# Use K-Means clustering for k = 
+# Use K-Means clustering for k = 2
 kMeansResult <- kmeans(newCoords, centers = 2, nstart = 25)
 
 # Plot color-coded result
 newCoords$Cluster_kmeans <- as.factor(kMeansResult$cluster)
 plotly::plot_ly(newCoords,x = ~PC1, y = ~PC2, z = ~PC3,
-        color = ~Cluster_kmeans,
-        marker = list(size = 3)) %>%
+                color = ~Cluster_kmeans,
+                marker = list(size = 3)) %>%
   layout(title = 'K-Means Clustering')
 
 # Add cluster assignment to original dataset
-dsBikeContract$Cluster <- factor(newCoords$Cluster_kmeans)
+dsContract$Cluster <- factor(newCoords$Cluster_kmeans)
 
 
 # Plot explanatory vars separately for each cluster
 # Plotly is known to have issues plotting within loops. Plotted without loop due to low amount of clusters
 # See https://github.com/ropensci/plotly/issues/273 for solution
 
-dsTemp <- subset(dsBikeContract, Cluster == 1)
+dsTemp <- subset(dsContract, Cluster == 1)
 plotly::plot_ly(dsTemp,x = ~PsychOwn, y = ~UseFreq, z = ~MinLength,
                 size = ~IntentContract,
                 marker = list(symbol = 'circle', sizemode = 'diameter'), sizes = c(1, 20))%>%
   layout(title = 'Survey Data, K-Means Cluster 1')
 
-dsTemp <- subset(dsBikeContract, Cluster == 2)
+dsTemp <- subset(dsContract, Cluster == 2)
 plotly::plot_ly(dsTemp,x = ~PsychOwn, y = ~UseFreq, z = ~MinLength,
                 size = ~IntentContract,
                 marker = list(symbol = 'circle', sizemode = 'diameter'), sizes = c(1, 20))%>%
@@ -214,7 +278,7 @@ plotly::plot_ly(dsTemp,x = ~PsychOwn, y = ~UseFreq, z = ~MinLength,
 # Determine optimal epsilon
 dbscan::kNNdistplot(newCoords[,1:3], k = 5) #knee occurs around eps = 1
 # Run dbscan
-dbscanResult <- fpc::dbscan(newCoords[,1:3], eps = 1, MinPts = 5)
+dbscanResult <- fpc::dbscan(newCoords[,1:3], eps = 0.8, MinPts = 5)
 # Assign clusters
 newCoords$Cluster_dbscan <- factor(dbscanResult$cluster)
 # Plot
@@ -222,13 +286,15 @@ plotly::plot_ly(newCoords,x = ~PC1, y = ~PC2, z = ~PC3,
                 color = ~Cluster_dbscan,
                 marker = list(size = 3))%>%
   layout(title = 'DBSCAN Clustering')
-dsBikeContract$Cluster_dbscan <- factor(newCoords$Cluster_dbscan)
+dsContract$Cluster_dbscan <- factor(newCoords$Cluster_dbscan)
 
 
 
 # Alternative: Cluster using the planes formed in the 3D PCA result (usually 2-4)
 # requires ransac?
 # may need to repurpose packages meant for computer vision feature detection
+
+
 
 #---------------------------------------------------
 #
